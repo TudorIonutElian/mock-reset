b58e9bcbebca5814d0e70ef125d5ad201db2b36b
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-03 21:19:06 +0200

    Remove forgoten files

--- dist/factories/Factory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Factory = /** @class */ (function () {
-    function Factory() {
-    }
-    return Factory;
-}());
-exports.default = Factory;
-//# sourceMappingURL=Factory.js.map
