c705cce92a2f0404d4afd35620287d78673ace6d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-04 03:43:15 +0200

    Add few methods for migration

--- models/Migration.ts
+++ models/Migration.ts
@@ -1,4 +1,18 @@
 class Migration {
+    private _tableName: string;
+    private _fields: any;
+
+    public addColumn(columnName: string, type: string, options: any = {}): void {
+        this._fields[columnName] = { type, options };
+    }
+
+    public convertFieldsToSQL(): string {
+        return '';
+    }
+
+    public run(): void {
+        console.log('Running migration for table: ' + this._tableName);
+    }
 
 }
 
